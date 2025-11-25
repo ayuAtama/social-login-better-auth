f158b7b0d625d956a37527092245c9611b7f074c
Wahyu Pratama <ayuramatama@gmail.com>
2025-11-25 16:47:15 +0700

    logout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { authClient } from "@/lib/auth-client";
+import { authClient, signOut, useSession } from "@/lib/auth-client";
 
 export default function Home() {
-  const { data: session, isPending, error, refetch } = authClient.useSession();
+  const { data: session, isPending, error, refetch } = useSession();
 
+  const fields = ["id", "email", "name"] as const;
+  const handleLogout = () => {
+    authClient.signOut();
+  };
   return (
     <div>
       <h1>Homepage</h1>
@@ -15,8 +19,45 @@ export default function Home() {
       {session?.user ? (
         <div>
           <p>Logged in as {session.user.email}</p>
-          <form action="/api/logout" method="post">
-            <button type="submit">Logout</button>
+          {/* map the session data */}
+          {Object.entries(session.user).map(([key, value]) => (
+            <p key={key}>
+              {key}: {String(value)}
+            </p>
+          ))}
+          <br />
+          {/* map spesific data */}
+          {(["id", "email", "name"] as Array<keyof typeof session.user>).map(
+            (key) => (
+              <p key={key}>
+                {key}: {String(session.user[key])}
+              </p>
+            )
+          )}
+
+          <br />
+
+          {fields.map((key) => (
+            <p key={key}>
+              {key}: {String(session.user[key])}
+            </p>
+          ))}
+
+          <br />
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <br />
+          {(Object.keys(session.user) as Array<keyof typeof session.user>).map(
+            (key) => (
+              <p key={key}>
+                {key}: {String(session.user[key])}
+              </p>
+            )
+          )}
+          <br />
+          <form action="/" method="post">
+            <button type="submit" onClick={handleLogout}>
+              Logout
+            </button>
           </form>
         </div>
       ) : (
